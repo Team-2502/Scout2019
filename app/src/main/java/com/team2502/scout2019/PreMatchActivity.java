085672b1ab5ecb67dd51bb7059458db9d772051b
team2502 <ryandalex1@outlook.com>
2019-07-18 18:13:36 -0500

    First attempt at layout for PreMatchActivity. Loads match number and team number automatically. No other functionality yet.

--- app/src/main/java/com/team2502/scout2019/PreMatchActivity.java
+++ app/src/main/java/com/team2502/scout2019/PreMatchActivity.java
@@ -3,27 +3,35 @@ package com.team2502.scout2019;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-
-public class MainActivity extends AppCompatActivity {
+public class PreMatchActivity extends AppCompatActivity {
 
     public static String timd_in_progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pre_match);
         timd_in_progress = "2502qm1,RA|";
-        ImportUtils.getMatchData(Constants.SERIAL_TO_SCOUT.get(((ApplicationInstance) this.getApplication()).getSp("scoutSerialNumber", "oof")), "match1");
-        TextView team_to_scout_view = findViewById(R.id.textView5);
+
+        int this_match = (((ApplicationInstance) this.getApplication()).getSp("lastMatch", 0)) + 1;
+        String this_match_string = "QM " + Integer.toString(this_match);
+
+        ImportUtils.getMatchData(Constants.SERIAL_TO_SCOUT.get(((ApplicationInstance) this.getApplication()).getSp("scoutSerialNumber", "oof")), this_match_string);
+
+        TextView team_to_scout_view = findViewById(R.id.teamToScout);
         team_to_scout_view.setText(Integer.toString(((ApplicationInstance) this.getApplication()).getSp("team", 0)));
-        Log.e("last_match sp", Integer.toString(((ApplicationInstance) this.getApplication()).getSp("lastMatch", 0)));
+
+        TextView match_number = findViewById(R.id.matchNumber);
+        match_number.setText(this_match_string);
+
     }
 
     public void startMatch(View view){
+        ImageView background = findViewById(R.id.imageView4);
         Intent intent = new Intent(this, MapActivity.class);
         intent.putExtra("com.team2502.scout2019.timd", timd_in_progress);
         startActivity(intent);
